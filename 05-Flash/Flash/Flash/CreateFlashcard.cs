c981d2bb6dbc451cf3bf2f8703e484ed507e0fa2
Young JIN <yjin@convoy-supply.com>
2024-02-21 15:43:00 -0800

    worked on Input Stack

---
+++ 05-Flash/Flash/Flash/CreateFlashcard.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Flash
+{
+    internal class CreateFlashcard
+    {
+        static string connectionString = "Data Source=(LocalDB)\\LocalDBDemo;Integrated Security=True";
+
+        internal static void GetCreateFlashcard()
+        {      
+            /*
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    connection.ChangeDatabase("DataBaseFlashCard");
+
+                    // Check if 'Stacks' table exists
+                    string checkTableQuery =
+                        @"SELECT COUNT(*) 
+                          FROM INFORMATION_SCHEMA.TABLES 
+                          WHERE TABLE_NAME = 'Stacks'";
+
+                    using (SqlCommand checkTableCommand = new SqlCommand(checkTableQuery, connection))
+                    {
+                        int tableCount = Convert.ToInt32(checkTableCommand.ExecuteScalar());
+                        if (tableCount == 0)
+                        {
+                            // If 'Stacks' table doesn't exist, create it
+                            string createStacksTableQuery =
+                                @"CREATE TABLE Stacks (
+                                  Stack_Primary_Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                                  Stack_Id INT NOT NULL,
+                                  Stack_Name NVARCHAR(50) NOT NULL,
+                                  Front NVARCHAR(50) NOT NULL,
+                                  Back NVARCHAR(50) NOT NULL
+                                )";
+                            using (SqlCommand createTableCommand = new SqlCommand(createStacksTableQuery, connection))
+                            {
+                                createTableCommand.ExecuteNonQuery();
+                                Console.WriteLine("Table 'Stacks' created successfully.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Table 'Stacks' already exists in database 'DataBaseFlashCard'.");
+                        }
+                    }
+
+                    // Check if 'Flashcards' table exists
+                    checkTableQuery =
+                        @"SELECT COUNT(*) 
+                          FROM INFORMATION_SCHEMA.TABLES 
+                          WHERE TABLE_NAME = 'Flashcards'";
+
+                    using (SqlCommand checkTableCommand = new SqlCommand(checkTableQuery, connection))
+                    {
+                        int tableCount = Convert.ToInt32(checkTableCommand.ExecuteScalar());
+                        if (tableCount == 0)
+                        {
+                            // If 'Flashcards' table doesn't exist, create it
+                            string createFlashcardsTableQuery =
+                                @"CREATE TABLE Flashcards (
+                                  Flashcard_Primary_Key INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                                  Stack_Id INT NOT NULL, 
+                                  Stack_Name NVARCHAR(50) NOT NULL,
+                                  Front NVARCHAR(50) NOT NULL,
+                                  Back NVARCHAR(50) NOT NULL,
+                                  CONSTRAINT FK_Stack_Id FOREIGN KEY (Stack_Id) REFERENCES Stacks(Stack_Primary_Id)
+                                )";
+                            using (SqlCommand createTableCommand = new SqlCommand(createFlashcardsTableQuery, connection))
+                            {
+                                createTableCommand.ExecuteNonQuery();
+                                Console.WriteLine("Table 'Flashcards' created successfully.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Table 'Flashcards' already exists in database 'FlashCardDataBase'.");
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            */
+        }
+    }
+}
---
+++ 05-Flash/Flash/Flash/WorkingStackMenu.cs
@@ -0,0 +1,71 @@
+﻿
+ using Flash;
+using Spectre.Console;
+
+namespace Flash
+{
+    internal class WorkingStackMenu
+    {
+        internal static void GetWorkingStackMenu(string currentWorkingStack)
+        {
+            //see available stacks 
+            try
+            {
+                Console.WriteLine($"Current Stack: {currentWorkingStack}\n");
+                // Create a list of Items, apply separate styles to each
+                var rows = new List<Text>(){
+                    new Text("-Type 0 to Return to Main Menu", new Style(Color.Red, Color.Black)),
+                    new Text("-Type 1 to Change Current Stack", new Style(Color.Green, Color.Black)),
+                    new Text("-Type 2 to View All Flashcards in Stack", new Style(Color.Blue, Color.Black)),
+                    new Text("-Type 3 to View X amount of cards in stack", new Style(Color.Purple, Color.Black)),
+                    new Text("-Type 4 to Create a Flashcard in current stack", new Style(Color.Orange3, Color.Black)),
+                    new Text("-Type 5 to Edit a Flashcard", new Style(Color.Red, Color.Black)),
+                    new Text("-Type 6 to Delete a Flashcard", new Style(Color.Green, Color.Black)),
+                    };
+
+                // Renders each item with own style
+                AnsiConsole.Write(new Rows(rows));
+
+                string commandTwo = Console.ReadLine();
+
+                switch (commandTwo)
+                {
+                    case "0":
+                        Console.WriteLine("\nGoodbye!\n");
+                        MainMenu.GetMainMenu();
+                        break;
+                    case "1":
+                        Console.WriteLine("Change Current Stack");
+                        ManageStacks.GetManageStacks();
+                        break;
+                    case "2":
+                        Console.WriteLine("View All Flashcards in Stack");                        
+                        break;
+                    case "3":
+                        Console.WriteLine("View X amount of cards in stack");
+                        break;
+                    case "4":
+                        Console.WriteLine("Create a Flashcard in current stack");
+                        CreateFlashcard.GetCreateFlashcard();
+                        break;
+                    case "5":
+                        Console.WriteLine("Edit a Flashcard");
+                        break;
+                    case "6":
+                        Console.WriteLine("Delete a Flashcard");
+                        break;
+
+                    default:
+                        Console.WriteLine("\nInvalid Command. Please type a number from 0 to 4.\n");
+                        break;
+                }
+                Console.ReadLine();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
